66ec3d0ea9b5361ccacbf5bf01310e8e1ceb3f30
SkilltissueMC <skilledtissued@gmail.com>
2025-04-26 00:02:56 +0100

    stuff?

--- src/main/java/skilltissue/effectpinger/config/config.java
+++ src/main/java/skilltissue/effectpinger/config/config.java
@@ -7,7 +7,7 @@ public class config extends MidnightConfig {
     public static final String EFFECT = "effect";
 
     //@Entry(category = EFFECT, name = "Sound ID") public static String soundID = SoundEvents.BLOCK_NOTE_BLOCK_PLING.getIdAsString();  (to be added)
-    @Entry(category = EFFECT, name = "Sound pitch", isSlider = true, min = 0F, max = 3, precision = 2) public static float pitch = 1;
-    @Entry(category = EFFECT, name = "Sound Volume", isSlider = true, min = 1F, max = 30) public static float volume = 15;
+    @Entry(category = EFFECT, name = "Sound pitch", isSlider = true, min = 0.5f, max = 5, precision = 10) public static float pitch = 1;
+    @Entry(category = EFFECT, name = "Sound Volume", isSlider = true, min = 0f, max = 2f, precision = 10) public static float volume = 1;
     @Entry(category = EFFECT, name = "Activation time", isSlider = true, min = 1F, max = 30) public static int activation = 15;
 }
