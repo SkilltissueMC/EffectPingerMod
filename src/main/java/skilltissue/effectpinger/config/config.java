a7251cafaf78437624005a7dae9f734fb8c60905
SkilltissueMC <skilledtissued@gmail.com>
2025-05-11 13:27:08 +0100

    mild config change

--- src/main/java/skilltissue/effectpinger/config/config.java
+++ src/main/java/skilltissue/effectpinger/config/config.java
@@ -2,8 +2,6 @@ package skilltissue.effectpinger.config;
 
 import com.google.common.collect.Lists;
 import eu.midnightdust.lib.config.MidnightConfig;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
 
 import java.util.List;
 
@@ -12,12 +10,12 @@ public class config extends MidnightConfig {
 
     @Entry(category = EFFECT, name = "Sound ID")
     public static String soundID = "minecraft:block.note_block.pling";
-    @Entry(category = EFFECT, name = "Sound pitch", isSlider = true, min = 0.5f, max = 5, precision = 10)
+    @Entry(category = EFFECT, name = "Sound pitch", min = 0.5f, max = 5)
     public static float pitch = 1;
-    @Entry(category = EFFECT, name = "Sound Volume", isSlider = true, min = 0f, max = 2f, precision = 10)
+    @Entry(category = EFFECT, name = "Sound Volume", min = 0f, max = 2f)
     public static float volume = 1;
     @Entry(category = EFFECT, name = "Activation time (seconds)", isSlider = true, min = 1F, max = 30)
-    public static int activation = 15;
+    public static int activation = 10;
     @Entry(category = EFFECT, name = "Effects to notify about")
     public static List<String> effects = Lists.newArrayList(
             "minecraft:strength",
